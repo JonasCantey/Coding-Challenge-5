d646446e6591299d66b5bb18942a6d054840f8e3
Jonas <jonashooncantey@gmail.com>
2025-02-09 14:37:34 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,10 @@
+//Task 1
+console.log("Task 1")
+
+let customer = {            //Declared an object named customer
+    name: "John Doe",           //Had to console.log each individual property due to interpolation issues.
+    age: 35,                   
+    email: "john.doe@gmail.com",
+};
+
+console.log(`Customer Profile: Name - ${customer.name}, Age - ${customer.age}, Email - ${customer.email}`)
