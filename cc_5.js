c934b12b4a3cc0c1a171ee5d41802a7b731d1a23
Jonas <jonashooncantey@gmail.com>
2025-02-09 14:38:29 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -8,3 +8,19 @@ let customer = {            //Declared an object named customer
 };
 
 console.log(`Customer Profile: Name - ${customer.name}, Age - ${customer.age}, Email - ${customer.email}`)
+
+
+//Task 2
+console.log("Task 2")
+
+let order = {               //Declared a new object named order
+    orderId: 12345,
+    totalAmount: 150,
+    status: "Processing",
+
+
+displayOrder() {           //Used a method called displayOrder which logged each part of the object
+    console.log(`Order details| Order ID: ${this.orderId}, Total Amount: ${this.totalAmount}, Status: ${this.status}`);
+}
+};
+order.displayOrder();      //Called the method to action
